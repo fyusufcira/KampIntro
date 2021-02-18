c4d351418c6f305dd164fb31919a092574aff7ee
Fatih Yusuf Çıra <fatih.cira@boun.edu.tr>
2021-02-18 16:28:25 +0300

    KampIntro

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager:Customer
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Added customer:"+Id);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted:"+Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            CustomerManager customerManager = new CustomerManager();
+            
+            customerManager.Add(customer1);
+            
+            customer1.Id = 1234324;
+            customer1.FirstName = "Yusuf";
+            customer1.LastName = "Çıra";
+            customer1.Age = 19;
+
+            Customer customer2 = new Customer();
+            customerManager.Add(customer2);
+            customer2.Id = 21311;
+            customer2.Age = 19;
+            customer2.FirstName = "Semiha";
+            customer2.LastName = "Savaşçı";
+
+
+        }
+    }
+}
